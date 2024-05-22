856d8242669fa41bc49a84a2eee9fc74dc75025b
blankenauksu <blankenw@ksu.edu>
2024-05-22 12:38:50 -0500

    a

--- np_1/+model_1/dynamic_resid.m
+++ np_1/+model_1/dynamic_resid.m
@@ -23,13 +23,13 @@ if T_flag
     T = model_1.dynamic_resid_tt(T, y, x, params, steady_state, it_);
 end
 residual = zeros(10, 1);
-    residual(1) = (T(9)*params(3)*exp(y(13))*y(6)*T(13)) - (y(6)*T(16));
-    residual(2) = (T(9)*params(3)*exp(y(14))*y(7)*T(14)) - (y(7)*T(16));
-    residual(3) = (params(4)*(1+params(3)*exp(y(17))*y(15)*T(10)-params(8))) - (T(12)/T(15));
+    residual(1) = (T(12)*params(3)*exp(y(13))*y(6)*T(16)) - (y(6)*T(19));
+    residual(2) = (T(12)*params(3)*exp(y(14))*y(7)*T(17)) - (y(7)*T(19));
+    residual(3) = (params(4)*(1+params(3)*exp(y(19))*y(15)*T(13)-params(8))) - (T(15)/T(18));
     residual(4) = (1) - (y(7)+y(5)+y(6));
-    residual(5) = (y(9)) - (exp(y(12))*y(5)*T(11)+y(1)*(1-params(8))-y(8));
-    residual(6) = (y(10)) - (exp(y(13))*y(6)*T(11));
-    residual(7) = (y(11)) - (exp(y(14))*y(7)*T(11));
+    residual(5) = (y(9)) - (exp(y(12))*y(5)*T(14)+y(1)*(1-params(8))-y(8));
+    residual(6) = (y(10)) - (exp(y(13))*y(6)*T(14));
+    residual(7) = (y(11)) - (exp(y(14))*y(7)*T(14));
     residual(8) = (y(12)) - (params(12)*y(2)+x(it_, 1));
     residual(9) = (y(13)) - (params(13)*y(3)+x(it_, 2));
     residual(10) = (y(14)) - (params(14)*y(4)+x(it_, 3));
--- np_1_checks/t_shocks.m
+++
@@ -1,8 +0,0 @@
-shocks;
-var a_0_shock=var_a_0;
-var a_1_shock=var_a_1;
-var a_2_shock=var_a_2;
-
-end;
-
-
